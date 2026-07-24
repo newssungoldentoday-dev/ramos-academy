d51e7fc5f5c9dd055a42dc8ef46875343e6b1688
BlueBixt <newssungoldentoday@gmail.com>
2026-07-25 07:35:36 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJM5O4EaF5_hlovuR1sW4JU8H5SDF48H8",
+  authDomain: "ramos-academy.firebaseapp.com",
+  projectId: "ramos-academy",
+  storageBucket: "ramos-academy.firebasestorage.app",
+  messagingSenderId: "279679701881",
+  appId: "1:279679701881:web:6d07354034a99512f9d5a8",
+  measurementId: "G-KNYC90Z0N1"
+};
